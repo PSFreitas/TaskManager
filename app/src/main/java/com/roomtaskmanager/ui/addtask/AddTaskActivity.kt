52f4f3d3f52258fedd4e871925f2fe526c1b6054
Paulo Sergio da Silva Freitas Junior <paulo.junior@ITBL00312.itbam.org.br>
2020-09-14 16:49:20 -0400

    feat: add Task title validation
    Disable Create Task button when task title is empty

--- app/src/main/java/com/roomtaskmanager/ui/addtask/AddTaskActivity.kt
+++ app/src/main/java/com/roomtaskmanager/ui/addtask/AddTaskActivity.kt
@@ -3,15 +3,14 @@ package com.roomtaskmanager.ui.addtask
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.roomtaskmanager.R
 import com.roomtaskmanager.data.TaskDatabase
-import com.roomtaskmanager.data.TaskEntity
 import com.roomtaskmanager.data.TaskRepositoryImp
 import com.roomtaskmanager.databinding.ActivityAddTaskBinding
 import kotlinx.android.synthetic.main.activity_add_task.*
-import java.util.*
 
 class AddTaskActivity : AppCompatActivity() {
 
@@ -45,7 +44,14 @@ class AddTaskActivity : AppCompatActivity() {
         super.onResume()
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        setupOnTextChangedListeners()
 
+    }
+
+    private fun setupOnTextChangedListeners() {
+        textInputEditText_task_name.doAfterTextChanged {
+            addTaskViewModel.verifyTaskTitle(it.toString())
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
--- app/src/main/java/com/roomtaskmanager/ui/addtask/AddTaskViewModel.kt
+++ app/src/main/java/com/roomtaskmanager/ui/addtask/AddTaskViewModel.kt
@@ -1,11 +1,21 @@
 package com.roomtaskmanager.ui.addtask
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.roomtaskmanager.data.TaskRepositoryImp
 
 class AddTaskViewModel(
     private val taskRepository: TaskRepositoryImp
 ) : ViewModel() {
+
+    private val _isDataValid: MutableLiveData<Boolean> = MutableLiveData()
+    val isDataValid: LiveData<Boolean> = _isDataValid
+
+
+    fun verifyTaskTitle(taskTitle: String) {
+        _isDataValid.value = taskTitle.isNotEmpty() && taskTitle.isNotBlank()
+    }
 
 
 }
