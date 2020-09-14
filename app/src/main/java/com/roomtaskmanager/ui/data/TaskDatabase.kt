10d44797603782bdbb80cb8ae11a9734c30ce7fb
Paulo Sergio da Silva Freitas Junior <paulo.junior@ITBL00312.itbam.org.br>
2020-09-14 14:31:16 -0400

    feat: implement TypeConverters annotation into TaskDatabase

--- app/src/main/java/com/roomtaskmanager/ui/data/TaskDatabase.kt
+++ app/src/main/java/com/roomtaskmanager/ui/data/TaskDatabase.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
+import com.roomtaskmanager.ui.converter.Converters
 
 @Database(
     entities = [TaskEntity::class],
     version = 1,
     exportSchema = false
 )
+@TypeConverters(Converters::class)
 abstract class TaskDatabase : RoomDatabase() {
 
     abstract val taskDao: TaskDao
