d1950acad17e3091ec6222d1c93a8dc49feb40be
Paulo Sergio da Silva Freitas Junior <paulo.junior@ITBL00312.itbam.org.br>
2020-09-14 18:03:27 -0400

    feat: retrieve all tasks
    Retrieve all tasks when enter the application and returns successfully from AddTaskActivity

---
+++ app/src/main/java/com/roomtaskmanager/ui/tasklist/ListTaskViewModel.kt
@@ -0,0 +1,34 @@
+package com.roomtaskmanager.ui.tasklist
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.roomtaskmanager.data.ResultData
+import com.roomtaskmanager.data.TaskEntity
+import com.roomtaskmanager.data.TaskRepositoryImp
+import com.roomtaskmanager.ui.addtask.Resource
+import kotlinx.coroutines.launch
+
+class ListTaskViewModel(
+    val taskRepositoryImp: TaskRepositoryImp
+) : ViewModel() {
+
+    private val _getAllTasksResult: MutableLiveData<Resource<List<TaskEntity>>> = MutableLiveData()
+    val getAllTaskResult: LiveData<Resource<List<TaskEntity>>> = _getAllTasksResult
+
+    fun getAllTasks() {
+        viewModelScope.launch {
+            val result = taskRepositoryImp.getAllTasks()
+
+            _getAllTasksResult.value = Resource.loading()
+
+            if (result is ResultData.Success) {
+                _getAllTasksResult.value = Resource.success(result.data)
+            } else {
+                _getAllTasksResult.value = Resource.error(Exception())
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/roomtaskmanager/ui/tasklist/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.roomtaskmanager.ui.tasklist
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.roomtaskmanager.R
+import com.roomtaskmanager.data.TaskDatabase
+import com.roomtaskmanager.data.TaskRepositoryImp
+import com.roomtaskmanager.ui.addtask.AddTaskActivity
+import com.roomtaskmanager.ui.addtask.Status
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    val taskListViewModel by lazy {
+        val taskRepositoryImp = TaskRepositoryImp(
+            TaskDatabase.getInstance(this)
+        )
+        ViewModelProvider(
+            this,
+            ListTaskViewModelFactory(
+                taskRepositoryImp
+            )
+        ).get(ListTaskViewModel::class.java)
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        getAllTasks()
+    }
+
+    private fun getAllTasks() {
+        taskListViewModel.getAllTasks()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setupOnClickListeners()
+        setupObservables()
+    }
+
+    private fun setupObservables() {
+        taskListViewModel.getAllTaskResult.observe(
+            this,
+            Observer {
+                if (it.status == Status.SUCCESS) {
+                }
+            }
+        )
+    }
+
+    private fun setupOnClickListeners() {
+        floatingActionButton_add_task.setOnClickListener {
+            val intent = Intent(this, AddTaskActivity::class.java)
+            startActivityForResult(
+                intent,
+                ADD_TASK_ACTIVITY
+            )
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == ADD_TASK_ACTIVITY) {
+            if (resultCode == Activity.RESULT_OK) {
+                taskListViewModel.getAllTasks()
+            }
+        }
+    }
+
+    companion object {
+        const val ADD_TASK_ACTIVITY = 1
+    }
+}
