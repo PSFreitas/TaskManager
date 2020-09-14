3c34d649e413f5cd3a628f5e2f1dd06642ffa3b0
Paulo Sergio da Silva Freitas Junior <paulo.junior@ITBL00312.itbam.org.br>
2020-09-14 11:29:11 -0400

    feat: show AddTaskActivity when FAB is clicked

---
+++ app/src/main/java/com/roomtaskmanager/AddTaskActivity.kt
@@ -0,0 +1,27 @@
+package com.roomtaskmanager
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+
+class AddTaskActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> onBackPressed()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+}
--- app/src/main/java/com/roomtaskmanager/MainActivity.kt
+++ app/src/main/java/com/roomtaskmanager/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.roomtaskmanager
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        setupOnClickListeners()
+    }
+
+    private fun setupOnClickListeners() {
+        floatingActionButton_add_task.setOnClickListener {
+            val intent = Intent(this,AddTaskActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
